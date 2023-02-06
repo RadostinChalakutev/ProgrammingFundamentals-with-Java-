1f4dffaabc1ce3e11c4f4c67499e3ca7bb7daee0
RadostinChalakutev <radostinfc@abv.bg>
2023-02-06 22:20:01 +0200

    BasicSyntaxConditionalStatementsandLoops\Lab\Even.java Initial commit

--- Methods/Excercise/AddAndSubtract.java
+++ Methods/Excercise/AddAndSubtract.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class AddAndSubtract {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int first = Integer.parseInt(scanner.nextLine());
+        int second = Integer.parseInt(scanner.nextLine());
         int third = Integer.parseInt(scanner.nextLine());
-
+        Add(first,second,third);
     }
 
     public static int Add(int first, int second, int third) {
