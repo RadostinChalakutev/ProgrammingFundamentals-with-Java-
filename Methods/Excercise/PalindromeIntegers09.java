db02c8cd70cabba39b5a39695c7c9545abc68407
RadostinChalakutev <radostinfc@abv.bg>
2023-02-06 22:10:16 +0200

    BasicSyntaxConditionalStatementsandLoops\Lab\Even.java Initial commit

---
+++ Methods/Excercise/PalindromeIntegers09.java
@@ -0,0 +1,11 @@
+package Methods.Excercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PalindromeIntegers09 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int number=Integer.parseInt(scanner.nextLine());
+    }
+}
