d446037c861e193bb84e610fd76813ec65c7a1c4
RadostinC <radostinfc@abv.bg>
2023-11-17 00:07:17 +0200

    14*89

--- Methods/Excercise/AddAndSubtract.java
+++ Methods/Excercise/AddAndSubtract.java
@@ -6,15 +6,15 @@ public class AddAndSubtract {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int first = Integer.parseInt(scanner.nextLine());
-        int second = Integer.parseInt(scanner.nextLine());
+
         int third = Integer.parseInt(scanner.nextLine());
         Add(first,second,third);
     }
 
-    public static int Add(int first, int second, int third) {
+    public static int Add(int first,  int third) {
 
 
-         int AddSum = first + second;
+         int AddSum= first+first ;
         int subtractSum=AddSum-third;
 
         System.out.println(subtractSum);
