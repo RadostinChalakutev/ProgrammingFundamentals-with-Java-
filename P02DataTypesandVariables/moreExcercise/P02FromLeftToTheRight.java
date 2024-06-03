23973077c1b780af9618b82b8c6c9ea361e5f8c3
RadostinC <radostinfc@abv.bg>
2024-06-03 16:09:35 +0300

    100/100

--- P02DataTypesandVariables/moreExcercise/P02FromLeftToTheRight.java
+++ P02DataTypesandVariables/moreExcercise/P02FromLeftToTheRight.java
@@ -5,5 +5,33 @@ import java.util.Scanner;
 public class P02FromLeftToTheRight {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
+
+        int number=Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i <number ; i++) {
+            double leftNumber=Double.parseDouble(scanner.next());
+            double rightNumber=Double.parseDouble(scanner.next());
+
+            int sum=0;
+            if (leftNumber>rightNumber){
+                double numFirst=Math.abs(leftNumber);
+
+                while(numFirst>0){
+
+                    sum+=numFirst%10;
+                    numFirst/=10;
+                }
+            }else {
+                double numSecond=Math.abs(rightNumber);
+
+                while(numSecond>0){
+                    sum+=numSecond%10;
+                    numSecond/=10;
+                }
+            }
+            System.out.println(Math.abs(sum));
+        }
+
+
     }
 }
---
+++ P02DataTypesandVariables/moreExcercise/P03FloatingEquality.java
@@ -0,0 +1,14 @@
+package P02DataTypesandVariables.moreExcercise;
+
+import java.util.Scanner;
+
+public class P03FloatingEquality {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+
+
+
+
+    }
+}
