eda3b8da39a339213c54072d564279c2d21d19b4
rados <radostnchalakutev@gmail.com>
2024-11-17 17:44:36 +0200

    Test

---
+++ P08ObjectAndClasses/excercise/Demo.java
@@ -0,0 +1,10 @@
+package P08ObjectAndClasses.excercise;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        int population=255879;
+
+        System.out.println(population);
+    }
+}
